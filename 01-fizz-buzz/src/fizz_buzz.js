b582535a70be6dee6fefb95650ced355897f5d09
Marcjanna Stopinska <marcjanna.stopinska@gmail.com>
2023-01-12 07:38:32 +0100

    feat: returns 100 elements

--- 01-fizz-buzz/src/fizz_buzz.js
+++ 01-fizz-buzz/src/fizz_buzz.js
@@ -1,5 +1,7 @@
 class FizzBuzz {
-
+  getNumbers() {
+    return Array.from(Array(100).keys());
+  }
 }
 
 module.exports = FizzBuzz;
--- 01-fizz-buzz/test/fizz_buzz.test.js
+++ 01-fizz-buzz/test/fizz_buzz.test.js
@@ -1,7 +1,15 @@
 const FizzBuzz = require("../src/fizz_buzz");
 
 describe("FizzBuzz", () => {
-  it("change_this_name", () => {
+  it("returns 100 elements", () => {
+    const fb = new FizzBuzz();
+
+    const numbers = fb.getNumbers();
+
+    expect(numbers.length).toBe(100);
+  });
+
+  it.skip("change_this_name", () => {
     new FizzBuzz();
 
     expect(true).toBe(true);
